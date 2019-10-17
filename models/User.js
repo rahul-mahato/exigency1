1904f1d692f23b7e9843d04d2e2acd0653724740
rocker785 <52028898+rocker785@users.noreply.github.com>
2019-10-17 12:53:30 +0530

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,102 +1,101 @@
-const mongoose = require('mongoose');
+const mongoose =require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    Fname: {
-        type: String,
-        required: true
+    Fname:{
+        type:String,
+        required:true
     },
-    Lname: {
-        type: String,
-        required: true
+    Lname:{
+        type:String,
+        required:true
     },
-    email: {
-        type: String,
-        required: true
+    email:{
+        type:String,
+        required:true
     },
-    password: {
-        type: String,
-        required: true
+    password:{
+        type:String,
+        required:true
     },
-    bloodgroup: {
-        type: String,
-        required: true
+    bloodgroup:{
+        type:String,
+        required:true
     },
-    DOB: {
-        type: Date,
+    DOB:{
+        type:Date,
         default: Date.now
     },
-    phoneno: {
-        type: Number,
-        required: true
+    phoneno:{
+        type:Number,
+        required:true
     },
-    vno: {
-        type: String,
-        required: true
+    vno:{
+        type:String,
+        required:true
     },
-    aadhaar: {
-        type: String,
-        required: true
+    macno:{
+        type:String,
+        default:"00:00:00:00"
     },
-    city: {
-        type: String,
-        required: true
+    aadhaar:{
+        type:String,
+        required:true
     },
-    add1: {
-        type: String,
-        required: true
+    city:{
+        type:String,
+        required:true
     },
-    add2: {
-        type: String
+    add1:{
+        type:String,
+        required:true
     },
-    state: {
-        type: String,
-        required: true
+    add2:{
+        type:String
     },
-    ecfn1: {
-        type: String,
-        required: true
+    state:{
+        type:String,
+        required:true
     },
-    ecln1: {
-        type: String,
-        required: true
+    ecfn1:{
+        type:String,
+        required:true
     },
-    ecpn1: {
-        type: Number,
-        required: true
+    ecln1:{
+        type:String,
+        required:true
     },
-    ecfn2: {
-        type: String,
-        required: true
+    ecpn1:{
+        type:Number,
+        required:true
     },
-    ecln2: {
-        type: String,
-        required: true
+    ecfn2:{
+        type:String,
+        required:true
     },
-    ecpn2: {
-        type: Number,
-        required: true
+    ecln2:{
+        type:String,
+        required:true
     },
-    ecfn3: {
-        type: String,
-        required: true
+    ecpn2:{
+        type:Number,
+        required:true
     },
-    ecln3: {
-        type: String,
-        required: true
+    ecfn3:{
+        type:String,
+        required:true
     },
-    ecpn3: {
-        type: Number,
-        required: true
+    ecln3:{
+        type:String,
+        required:true
     },
-    authNo: {
-        type: Number,
-        required: true
+    ecpn3:{
+        type:Number,
+        required:true
     },
-    IOTmac: {
-        type: Number,
-
+    authNo:{
+        type:Number,
     }
 });
 
-const User = mongoose.model('User', UserSchema);
+const User= mongoose.model('User',UserSchema);
 module.exports = User;
